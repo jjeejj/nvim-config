cb5389025759cf082f657e37d0b4058cb6dcb074
jjeejj <jwj@wenjunjiang.com>
2022-08-05 22:20:45 +0800

    feat: basic 基础配置

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
--- set options
-vim.o.number = true
+require("basic")
 -- Packer 插件插件管理
+
 require("plugins")
 -- 插件的配置
 require("plugin-config.nvim-tree")
---
+++ lua/basic.lua
@@ -0,0 +1,25 @@
+-- utf-8
+vim.g.encoding = "UTF-8"
+
+-- 展示行号
+vim.o.number = true
+
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+
+-- 好亮所在行
+vim.wo.cursorline = true
+
+
+-- 显示 tabline
+ vim.o.showtabline = 2
+
+ -- 关闭底部默认状态显示
+vim.o.showmode = false
+
+-- 留出一列给图标显示
+vim.o.signcolumn = "yes"
+
+-- 右侧行长度参考线
+vim.wo.colorcolumn = "80"
