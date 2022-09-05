a5397bb003495cfdf92e539293495ea949ec97f0
姜文俊 <wenjunjiang@aquanliang.com>
2022-09-05 21:51:13 +0800

    feat: base tab size

---
+++ lua/basic.lua
@@ -0,0 +1,38 @@
+-- utf-8
+vim.g.encoding = "UTF-8"
+
+-- 展示行号
+vim.o.number = true
+
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+
+-- 好亮所在行
+vim.wo.cursorline = true
+
+
+-- 显示 tabline
+ vim.o.showtabline = 2
+
+ -- 关闭底部默认状态显示
+vim.o.showmode = false
+
+-- 留出一列给图标显示
+vim.o.signcolumn = "yes"
+
+-- 右侧行长度参考线
+vim.wo.colorcolumn = "80"
+
+-- tab 4 个空格
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.bo.softtabstop = 4
+vim.o.shiftround = true
+
+-- 空格代替 tab
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+-- 缩进
