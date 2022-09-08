60b00a29e90820010e2316b8e08b572bc10e1ec5
jjeejj <jwj@wenjunjiang.com>
2022-09-08 19:07:12 +0800

    feat: lus lsp

--- lua/lsp/config/lua.lua
+++ lua/lsp/config/lua.lua
@@ -1,3 +1,29 @@
 return {
-    
+    on_setup = function(server)
+        server:setup({
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                    -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file('', true),
+                        checkThirdParty = false,
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
+        })
+    end,
+    on_attach = function(client, bufnr)
+        
+    end,
 }
