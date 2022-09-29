fd1399e65fd74696da00c32bcc72fc8beeca649d
jjeejj <jwj@wenjunjiang.com>
2022-09-29 09:59:34 +0800

    fix: nvim tree setup

--- lua/lsp/config/lua.lua
+++ lua/lsp/config/lua.lua
@@ -4,37 +4,37 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 return {
-    on_setup = function(server)
-        server:setup({
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = "LuaJIT",
-                        path = runtime_path,
-                    },
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                    -- Make the server aware of Neovim runtime files
-                        library = vim.api.nvim_get_runtime_file('', true),
-                        checkThirdParty = false,
-                    },
-                    -- Do not send telemetry data containing a randomized but unique identifier
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
-            on_attach = function(client, bufnr)
-            -- 禁用格式化功能，交给专门插件插件处理
-            client.resolved_capabilities.document_formatting = false
-            client.resolved_capabilities.document_range_formatting = false
-            require("keymappings").lspList(bufnr)
-            -- 保存时自动格式化
-            vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
-            end,
-        })
-    end,
+	on_setup = function(server)
+		server:setup({
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+						path = runtime_path,
+					},
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
+					-- Do not send telemetry data containing a randomized but unique identifier
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+			on_attach = function(client, bufnr)
+				-- 禁用格式化功能，交给专门插件插件处理
+				client.resolved_capabilities.document_formatting = false
+				client.resolved_capabilities.document_range_formatting = false
+				require("keymappings").lspList(bufnr)
+				-- 保存时自动格式化
+				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			end,
+		})
+	end,
 }
--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,48 +1,48 @@
 -- 侧边栏配置
 local status, nvim_tree = pcall(require, "nvim-tree")
 if not status then
-    vim.notify("not found nvim-tree")
-    return
+	vim.notify("not found nvim-tree")
+	return
 end
 
 -- 列表操作快捷键
-local list_keys = require('keymappings').nvimTreeList
+local list_keys = require("keymappings").nvimTreeList
 -- 设置配置项
 nvim_tree.setup({
-    git = {
-        enable = false,
-    },
-    sort_by = "case_sensitive",
-    update_cwd = true,
-    respect_buf_cwd = true,
-    open_on_tab = true,
-    update_focused_file = {
-        enable = true,
-        update_cwd = false,
-        ignore_list = {},
-        update_root = true,
-    },
-    filters = {
-        dotfiles = false,
-    },
-    view = {
-        width = 30,
-        side = "left",
-        adaptive_size = true,
-        signcolumn = "yes",
-        mappings = {
-            custom_only = false,
-            list = list_keys,
-        }
-    },
-    system_open = {
-        cmd = "open"
-    },
-    action = {
-        open_file = {
-            quit_on_open = false,
-        },
-    }
+	git = {
+		enable = false,
+	},
+	sort_by = "case_sensitive",
+	update_cwd = true,
+	respect_buf_cwd = true,
+	open_on_tab = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+		ignore_list = {},
+		update_root = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	view = {
+		width = 30,
+		side = "left",
+		adaptive_size = true,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = list_keys,
+		},
+	},
+	system_open = {
+		cmd = "open",
+	},
+	actions = {
+		open_file = {
+			quit_on_open = false,
+		},
+	},
 })
 -- 自动关闭
 vim.cmd([[
