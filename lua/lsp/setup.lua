14c03737ca6e26e91805e30648f641f2d08c9a47
jjeejj <jwj@wenjunjiang.com>
2023-03-28 23:47:56 +0800

    feat: lua mason lspconfig

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,11 +1,22 @@
-local lspconfig = require("mason-lspconfig")
+local status , mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status then
+    vim.notify("not found mason lspconfig")
+    return
+end
+
+local status, lspconfig = pcall(require, "lspconfig")
+if not status then
+    vim.notify("not found lspconfig")
+    return
+end
+
 
 -- 加载对应的 lsp
-lspconfig.setup_handlers({
+mason_lspconfig.setup_handlers({
     function(server_name)
-        require("lspconfig")[server_name].setup()
+        lspconfig[server_name].setup()
     end,
     ["lua_ls"] = function ()
-        require("lsp.config.lua")()
+        require("lsp.config.lua").on_setup(lspconfig["lua_ls"])
     end,
 })
