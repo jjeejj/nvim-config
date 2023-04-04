1d0341fbf9f6b126a6ee9421d3838b4e1405cfbe
jjeejj <jwj@wenjunjiang.com>
2023-04-04 12:31:36 +0800

    chore: fmt

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -1,57 +1,57 @@
 local status, dashboard = pcall(require, "dashboard")
 if not status then
-	vim.notify("dashboard-nvim not installed", vim.log.levels.WAR)
-	return
+    vim.notify("dashboard-nvim not installed", vim.log.levels.WAR)
+    return
 end
 
 dashboard.setup({
-	theme = "doom",
+    theme = "doom",
     config = {
         packages = {
-            enable = true
+            enable = true,
         },
         header = {
-		    [[]],
             [[]],
             [[]],
-		    [[██╗    ██╗███████╗███╗   ██╗         ██╗██╗   ██╗███╗   ██╗]],
-		    [[██║    ██║██╔════╝████╗  ██║         ██║██║   ██║████╗  ██║]],
-		    [[██║ █╗ ██║█████╗  ██╔██╗ ██║         ██║██║   ██║██╔██╗ ██║]],
-		    [[██║███╗██║██╔══╝  ██║╚██╗██║    ██   ██║██║   ██║██║╚██╗██║]],
-		    [[╚███╔███╔╝███████╗██║ ╚████║    ╚█████╔╝╚██████╔╝██║ ╚████║]],
-		    [[ ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝     ╚════╝  ╚═════╝ ╚═╝  ╚═══╝]],
-		    [[]],
-		    [[]],
-	    },
-	    footer = {
-		    "github.com:jjeejj/nvim-config                  ",
-	    },
-	    center = {
-		    {
-			    icon = "       ",
-			    desc = "Projects                            ",
-			    action = "Telescope projects",
-		    },
-		    {
-			    icon = "       ",
-			    desc = "Recently files                      ",
-			    action = "Telescope oldfiles",
-		    },
-		    {
-			    icon = "       ",
-			    desc = "Edit keymappings                    ",
-			    action = "edit ~/.config/nvim/lua/keymappings.lua",
-		    },
-		    {
-			    icon = "       ",
-			    desc = "Edit Projects                       ",
-			    action = "edit ~/.local/share/nvim/project_nvim/project_history",
-		    },
+            [[]],
+            [[██╗    ██╗███████╗███╗   ██╗         ██╗██╗   ██╗███╗   ██╗]],
+            [[██║    ██║██╔════╝████╗  ██║         ██║██║   ██║████╗  ██║]],
+            [[██║ █╗ ██║█████╗  ██╔██╗ ██║         ██║██║   ██║██╔██╗ ██║]],
+            [[██║███╗██║██╔══╝  ██║╚██╗██║    ██   ██║██║   ██║██║╚██╗██║]],
+            [[╚███╔███╔╝███████╗██║ ╚████║    ╚█████╔╝╚██████╔╝██║ ╚████║]],
+            [[ ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝     ╚════╝  ╚═════╝ ╚═╝  ╚═══╝]],
+            [[]],
+            [[]],
+        },
+        footer = {
+            "github.com:jjeejj/nvim-config                  ",
+        },
+        center = {
+            {
+                icon = "       ",
+                desc = "Projects                            ",
+                action = "Telescope projects",
+            },
+            {
+                icon = "       ",
+                desc = "Recently files                      ",
+                action = "Telescope oldfiles",
+            },
+            {
+                icon = "       ",
+                desc = "Edit keymappings                    ",
+                action = "edit ~/.config/nvim/lua/keymappings.lua",
+            },
+            {
+                icon = "       ",
+                desc = "Edit Projects                       ",
+                action = "edit ~/.local/share/nvim/project_nvim/project_history",
+            },
             {
                 icon = "       ",
-                desc = "Change Theme                        ",    
-                action = "Telescope colorscheme             ",    
+                desc = "Change Theme                        ",
+                action = "Telescope colorscheme             ",
             },
-        },   
-	},
+        },
+    },
 })
--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -1,32 +1,32 @@
 local status, telescope = pcall(require, "telescope")
 if not status then
-	vim.notify("telescope.nvim not installed", vim.log.levels.WARN)
-	return
+    vim.notify("telescope.nvim not installed", vim.log.levels.WARN)
+    return
 end
 
 telescope.setup({
-	defaults = {
-		initial_mode = "insert",
-		layout_strategy = "horizontal",
-		mappings = require("keymappings").telescopeList,
-	},
-	pickers = {
-		find_files = {
-			theme = "ivy",
-		},
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			overrode_file_sorter = true,
-			case_mode = "smart_case",
-		},
-		media_files = {
-			filetypes = { "png", "webp", "jpg", "jpeg" },
-			find_cmd = "rg",
-		},
-	},
+    defaults = {
+        initial_mode = "insert",
+        layout_strategy = "horizontal",
+        mappings = require("keymappings").telescopeList,
+    },
+    pickers = {
+        find_files = {
+            theme = "ivy",
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            overrode_file_sorter = true,
+            case_mode = "smart_case",
+        },
+        media_files = {
+            filetypes = { "png", "webp", "jpg", "jpeg" },
+            find_cmd = "rg",
+        },
+    },
 })
 
 -- telescope extensions
